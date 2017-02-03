f92fe89d3170623e8617696ed6e340cdd2652ff0
sky <diufanshu@gmail.com>
2017-02-03 23:49:38 +0800

    add src files

---
+++ src/Extract.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.*;
+
+public class Extract {
+
+	public static void main(String[] args) {
+		try {  
+            InputStream is = new FileInputStream("lib/list");  
+            BufferedReader reader = new BufferedReader(  
+                    new InputStreamReader(is));  
+            String str = null;  
+            while (true) {  
+                str = reader.readLine();  
+                if(str!=null){
+                	File dir = new File("lib/" + str);
+            		File[] filesList = dir.listFiles();
+            		for (File file : filesList) {
+            		    if (file.isFile()) {
+            		    	 Path path = Paths.get("data/" + str);
+            		         //if directory exists?
+            		         if (!Files.exists(path)) {
+            		             try {
+            		                 Files.createDirectories(path);
+            		             } catch (IOException e) {
+            		                 //fail to create directory
+            		                 e.printStackTrace();
+            		             }
+            		         }
+            		    	ProcessFile.wash("lib/" + str + "/" + file.getName(), "data/" + str + "/" + file.getName());
+            		    }
+            		}
+                }
+                else  
+                    break;  
+            }  
+            reader.close();  
+            is.close();
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }
+		
+		
+
+	}
+
+}
---
+++ src/ProcessFile.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.*;
+
+public class ProcessFile {
+	public static void wash(String inputfile, String outputfile){
+//		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+//		Path currentRelativePath = Paths.get("");
+//		String s = currentRelativePath.toAbsolutePath().toString();
+//		System.out.println("Current relative path is: " + s);
+//		System.out.println(inputfile);
+//		System.out.println(outputfile);
+		List<String> list = new ArrayList<String>();
+		Path nfile = Paths.get(outputfile);
+        try
+        {
+            String encoding = "UTF-8";
+            File file = new File(inputfile);
+            if (file.isFile() && file.exists())
+            { // 判断文件是否存在
+                InputStreamReader read = new InputStreamReader(
+                        new FileInputStream(file), encoding);// 考虑到编码格式
+                BufferedReader bufferedReader = new BufferedReader(read);
+                String lineTxt = "";
+                int nbLine = 1;
+                int nbTask = 0;                
+                
+                while ((lineTxt = bufferedReader.readLine()) != null)
+                {
+//                    list.add(lineTxt);
+                    if (nbLine == 6) {
+						// get number of jobs
+                    	String[] temp = lineTxt.split(":  ");
+//                    	System.out.println(temp[1]);
+                    	list.add(temp[1]);
+                    	nbTask = Integer.parseInt(temp[1]);
+//                    	System.out.println(nbTask);
+					}
+                    
+                    if (nbLine == 9) {
+						// get renewable resources
+						String[] temp = lineTxt.split(":  ");
+						String[] temp2 = temp[1].split(" ");
+//						System.out.println(temp2[0]);
+						list.add(temp2[0]);
+//						for (String string : temp) {
+//							System.out.println(string);
+//						}
+					}
+                    
+                    if (nbLine == 10) {
+						// get nonrenewable resources
+						String[] temp = lineTxt.split(":  ");
+						String[] temp2 = temp[1].split(" ");
+//						System.out.println(temp2[0]);
+						list.add(temp2[0]);
+						list.add(" ");
+					}
+                    
+                    if (nbLine >= 19 && nbLine < 19 + nbTask) {
+						// get main data
+//                    	System.out.println(lineTxt);
+                    	list.add(lineTxt);
+					}
+                    
+                    if (nbLine >= 19 + nbTask + 4)  {
+						if (lineTxt.contains("*") || lineTxt.contains("R")) {
+//							System.out.println("continue");
+						}else {
+//							list.add(" ");
+							list.add(lineTxt);
+//							System.out.println(lineTxt);
+						}
+					}
+                    
+                    nbLine += 1;
+                }
+                bufferedReader.close();
+                read.close();
+//                list.add(19 + nbTask + 4, " ");
+            }
+            else
+            {
+                System.out.println("找不到指定的文件");
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("读取文件内容出错");
+            e.printStackTrace();
+        }
+        
+//        System.out.print(list);
+        
+        
+        
+        try {
+        	list.add(3, " ");
+            list.add(4, list.get(list.size()-1));
+            list.remove(list.size()-1);        	
+			Files.write(nfile,list,Charset.forName("UTF-8"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
